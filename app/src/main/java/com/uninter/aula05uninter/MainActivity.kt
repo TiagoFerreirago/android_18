202c74ca7dea5961cd70d5359197d82fde83b6ff
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2024-06-22 13:46:21 -0300

    Aula05AndroidUninter Part2

---
+++ app/src/main/java/com/uninter/aula05uninter/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.uninter.aula05uninter
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnLogin).setOnClickListener{
+            startActivity(Intent(this,telarecuperarasenhaaula05::class.java))
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/uninter/aula05uninter/telarecuperarasenhaaula05.kt
@@ -0,0 +1,17 @@
+package com.uninter.aula05uninter
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class telarecuperarasenhaaula05 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_telarecuperarasenhaaula05)
+
+
+    }
+}
