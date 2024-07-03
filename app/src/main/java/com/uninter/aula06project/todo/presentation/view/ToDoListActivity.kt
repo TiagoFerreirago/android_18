d7feba023a107abf9ecf1508dc3f6c7462794bfd
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2024-07-03 17:40:07 -0300

    Aula 07 template e estrutura lista dinamica

--- app/src/main/java/com/uninter/aula06project/todo/presentation/view/ToDoListActivity.kt
+++ app/src/main/java/com/uninter/aula06project/todo/presentation/view/ToDoListActivity.kt
@@ -2,20 +2,33 @@ package com.uninter.aula06project.todo.presentation.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.uninter.aula06project.R
 import com.uninter.aula06project.todo.data.repository.ToDoMemoryRepository
 import com.uninter.aula06project.todo.domain.entity.ToDo
+import com.uninter.aula06project.todo.presentation.viewmodel.ToDoViewModel
 
 class ToDoListActivity : AppCompatActivity() {
+
+    lateinit var rcToDoList :RecyclerView
+    val viewModel by lazy{
+        ViewModelProvider(this).get(ToDoViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_to_do_list)
 
+        rcToDoList = findViewById<RecyclerView>(R.id.rcToDo)
+//verificar se tem atualização na lista e instancia o load
+        viewModel.todos.observe(this){
+            rcToDoList.layoutManager = LinearLayoutManager(this)
+            rcToDoList.adapter = ToDoListAdapter(it)
+        }
 
-        var rcToDoList = findViewById<RecyclerView>(R.id.rcToDo)
-        rcToDoList.layoutManager = LinearLayoutManager(this)
-        rcToDoList.adapter = ToDoListAdapter(ToDoMemoryRepository.getAll())
+
+
     }
 }
---
+++ app/src/main/java/com/uninter/aula06project/todo/presentation/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,24 @@
+package com.uninter.aula06project.todo.presentation.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.uninter.aula06project.todo.domain.entity.ToDo
+import com.uninter.aula06project.todo.domain.usecase.ToDoUseCase
+import com.uninter.aula06project.todo.domain.usecase.interfaces.IToDoUseCase
+
+class ToDoViewModel : ViewModel() {
+
+    private val useCase : IToDoUseCase = ToDoUseCase();//conectando com o usecase
+    var todos = MutableLiveData<List<ToDo>>();//atualiza a lista de forma dinamica
+
+
+
+    init{
+        Thread(Runnable {    LoadTodo(); }).start()// o método e atualizado em tempo real sem travar a tela
+
+    }
+    fun LoadTodo(){
+        todos.postValue(useCase.getAll())
+
+    }
+}
